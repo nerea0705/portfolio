2c8d98d5340653ba150047df294a55d6f964007d
nmarco <nmarco@epigram.es>
2025-11-19 15:46:00 +0100

    clean app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   imports: [
     CommonModule,
-    RouterOutlet,
-    TranslateModule
+    RouterOutlet
   ],
   templateUrl: './app.html',
   styleUrl: './app.less'
 })
-export class App {
-  constructor(private translate: TranslateService) {
-    this.translate.use('es');
-  }
-}
+export class App {}
